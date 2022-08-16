ee21bb5c5e509d5ef689e4c00f12c7ed0b1e0063
Matias Capuano <maticapuano97@gmail.com>
2022-08-16 15:44:42 -0300

    chore(consumer): improve force retries

--- poc-consumer/src/modules/pull/domain/jobs/consumers/pull-tokenization.consumer.ts
+++ poc-consumer/src/modules/pull/domain/jobs/consumers/pull-tokenization.consumer.ts
@@ -5,24 +5,24 @@ import { TodoTokenizationConsumerInterface } from '../../interfaces/todo.interfa
 
 @Processor(PullEventQueue.TOKENIZATION)
 export class PullTokenizationConsumer {
-  private retryNro = 0;
-
   @Process()
   public async execute(
     job: Job<TodoTokenizationConsumerInterface>,
   ): Promise<void> {
-    // job.data.data.forEach(() => {
-    //   if (this.retryNro != 5) {
-    //     this.retryNro = job.attemptsMade;
+    let retryNro = job.attemptsMade;
 
-    //     console.log(`Job: ${job.id} retry nro #${job.attemptsMade}`);
+    job.data.data.forEach(() => {
+      if (retryNro != 3) {
+        console.log(`Job: ${job.id} retry nro #${job.attemptsMade}`);
 
-    //     throw new Error('Something went wrong');
-    //   }
-    // });
+        throw new Error('Something went wrong');
+      }
+    });
 
     console.log(`retry nro ${job.attemptsMade} processed`);
 
     console.log(job.data.data.find((todo) => todo.id === 1));
+
+    retryNro = 0;
   }
 }
